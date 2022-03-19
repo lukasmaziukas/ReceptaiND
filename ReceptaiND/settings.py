2c95e233424cac5af3e6c04014729e749bc3990a
lukasmaziukas <lukasmaz18@gmail.com>
2022-03-19 15:35:54 +0200

    secret

--- ReceptaiND/settings.py
+++ ReceptaiND/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ty5u06v!v-5b-ku8*5w%d2-h1hde-iu*9#p@9=c4mqz#=i=hs'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
